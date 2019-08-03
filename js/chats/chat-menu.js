8783e298189a1fb68d6d924da0d578c0c3d69333
cvdat2097 <cvdat2097@gmail.com>
2019-08-03 22:43:10 +0700

    deactive chat-menu css class when being toggled

--- js/chats/chat-menu.js
+++ js/chats/chat-menu.js
@@ -1,25 +1,7 @@
-// TODO: Enhance this mechanism
-// ISSUES: New chat-container continously added (via lazy loading); chat-menu toggled
 let activeChatMenu = null;
 let inactiveChatStates = [];
 
-const chatPropertyButtons = document.getElementsByClassName('chat-property');
-for (const btn of chatPropertyButtons) {
-    const chatMenu = btn.getElementsByClassName('chat-menu')[0];
-    const chatStates = btn.getElementsByClassName('chat-state');
-
-    btn.addEventListener('click', () => {
-        chatMenu.className += ' chat-menu-active';
-        activeChatMenu = chatMenu;
-
-        for (const state of chatStates) {
-            state.className += ' chat-state-inactive';
-        }
-        inactiveChatStates = chatStates;
-    });
-}
-
-document.body.addEventListener('click', () => {
+const closeAllChatPropeties = () => {
     activeChatMenu.className = activeChatMenu.className.replace(
         /\schat-menu-active/g,
         ''
@@ -28,4 +10,28 @@ document.body.addEventListener('click', () => {
     for (const state of inactiveChatStates) {
         state.className = state.className.replace(/\schat-state-inactive/g, '');
     }
+};
+
+const chatPropertyButtons = document.getElementsByClassName('chat-property');
+for (const btn of chatPropertyButtons) {
+    const chatMenu = btn.getElementsByClassName('chat-menu')[0];
+    const chatStates = btn.getElementsByClassName('chat-state');
+
+    btn.addEventListener('click', () => {
+        if (chatMenu.className.indexOf('chat-menu-active') !== -1) {
+            closeAllChatPropeties();
+        } else {
+            chatMenu.className += ' chat-menu-active';
+            activeChatMenu = chatMenu;
+
+            for (const state of chatStates) {
+                state.className += ' chat-state-inactive';
+            }
+            inactiveChatStates = chatStates;
+        }
+    });
+}
+
+document.body.addEventListener('click', () => {
+    closeAllChatPropeties();
 });
